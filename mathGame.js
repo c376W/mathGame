9c4143325454634332a019f02f3c4fc554d19460
c376W <mailfzc123@gmail.com>
2019-08-03 16:00:39 -0700

    Adding questions to the panel.

--- mathGame.js
+++ mathGame.js
@@ -2,18 +2,51 @@ let playing=false;
 let startReset=document.querySelector("#startReset");
 let scoreValue=document.querySelector("#scoreValue");
 let seconds=document.querySelector("#seconds");
-let time=60;
+let formula=document.querySelector("#formula");
+let xText=document.querySelector("#x");
+let yText=document.querySelector("#y");
+let cal=document.querySelector("#cal");
+let time=4;
 let score=0;
+let correct=0;
+let x,y=0;
+let calD=["+","-","x","/"];
+let i=0;
 let gameOver=document.querySelector("#gameOver");
 
 startReset.addEventListener("click",function(){
 	if(playing===false){
 		playing=true;
+		x=Math.floor(20*Math.random());
+		y=Math.floor(20*Math.random());
+		xText.innerHTML=x;
+		yText.innerHTML=y;
+		i=Math.floor(4*Math.random());
+		cal.innerHTML=calD[i];
+		switch(calD[i]){
+			case "+":
+				correct=x+y;
+				break;
+			case "-":
+				correct=x-y;
+				break;
+			case "x":
+				correct=x*y;
+				break;
+			case "/":
+				correct=x/y;
+				break;
+		}
+		console.log(correct);
+
+
+
 		startReset.innerText="Reset Game";
 		seconds.innerText="60";
 		countDown();
 	}else{
 		playing=false;
+		formula.innerHTML="";
 		startReset.innerText="Start Game";
 		location.reload();
 
