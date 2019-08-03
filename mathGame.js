6b10bcf826789b3ef546b8ae41d2451f4dbe43b1
c376W <mailfzc123@gmail.com>
2019-08-02 20:14:15 -0700

    Adding countdown feature.

--- mathGame.js
+++ mathGame.js
@@ -2,12 +2,16 @@ let playing=false;
 let startReset=document.querySelector("#startReset");
 let scoreValue=document.querySelector("#scoreValue");
 let seconds=document.querySelector("#seconds");
+let time=60;
+let score=0;
+let gameOver=document.querySelector("#gameOver");
 
 startReset.addEventListener("click",function(){
 	if(playing===false){
 		playing=true;
 		startReset.innerText="Reset Game";
 		seconds.innerText="60";
+		countDown();
 	}else{
 		playing=false;
 		startReset.innerText="Start Game";
@@ -15,3 +19,26 @@ startReset.addEventListener("click",function(){
 
 	}
 })
+
+//Functions
+
+//Countdown
+const countDown=()=>{
+	let action=setInterval(()=>{
+		time--;
+		seconds.innerHTML=time;
+		stopTime(time,action);
+	},1000)
+
+}
+
+//Clear count down
+
+const stopTime=(time,action)=>{
+	if(time===0){
+		clearInterval(action);
+		gameOver.style.display="flex";
+		gameOver.childNodes[3].childNodes[1].innerHTML=score;
+		
+	}
+}
