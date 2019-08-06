2f57dce01459e58ff6fa3ca4f9e0bda5bdab26bb
c376W <mailfzc123@gmail.com>
2019-08-05 19:05:20 -0700

    Adding choices.

--- mathGame.js
+++ mathGame.js
@@ -6,11 +6,13 @@ let formula=document.querySelector("#formula");
 let xText=document.querySelector("#x");
 let yText=document.querySelector("#y");
 let cal=document.querySelector("#cal");
+let choices=document.querySelector("#choices");
 let time=4;
 let score=0;
 let correct=0;
 let x,y=0;
 let calD=["+","-","x","/"];
+let choiceArr=[1,3,5,7];
 let i=0;
 let gameOver=document.querySelector("#gameOver");
 
@@ -18,11 +20,12 @@ startReset.addEventListener("click",function(){
 	if(playing===false){
 		playing=true;
 		x=Math.floor(20*Math.random());
-		y=Math.floor(20*Math.random());
+		y=Math.floor(20*Math.random()+1);
 		xText.innerHTML=x;
 		yText.innerHTML=y;
 		i=Math.floor(4*Math.random());
 		cal.innerHTML=calD[i];
+
 		switch(calD[i]){
 			case "+":
 				correct=x+y;
@@ -34,10 +37,22 @@ startReset.addEventListener("click",function(){
 				correct=x*y;
 				break;
 			case "/":
-				correct=x/y;
+				correct=Math.round(x/y*100)/100;
 				break;
 		}
+		let correct_index=choiceArr[Math.floor(4*Math.random())];
+		choices.childNodes[correct_index].innerHTML=correct;
+		for(let elem of choices.childNodes){
+			if(!elem.innerHTML){
+				if(correct%1!==0){
+					elem.innerHTML=Math.floor(100*Math.random()-10)/10;
+				}else{
+					elem.innerHTML=Math.floor(100*Math.random()-10);
+				}
+			}
+		}
 		console.log(correct);
+		console.log(choices.childNodes[1]);
 
 
 
