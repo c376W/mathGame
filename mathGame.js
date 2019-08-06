a1558aba5483e7440cbca898677642165634eb7b
c376W <mailfzc123@gmail.com>
2019-08-06 12:37:59 -0700

    Adding right answer, changing javascript logic.

--- mathGame.js
+++ mathGame.js
@@ -7,62 +7,30 @@ let xText=document.querySelector("#x");
 let yText=document.querySelector("#y");
 let cal=document.querySelector("#cal");
 let choices=document.querySelector("#choices");
-let time=4;
+let wrong=document.querySelector("#wrong");
+let time=60;
 let score=0;
+let chosenFlag=false;
 let correct=0;
 let x,y=0;
 let calD=["+","-","x","/"];
 let choiceArr=[1,3,5,7];
 let i=0;
 let gameOver=document.querySelector("#gameOver");
+var choiceValues=[];
 
 startReset.addEventListener("click",function(){
 	if(playing===false){
 		playing=true;
-		x=Math.floor(20*Math.random());
-		y=Math.floor(20*Math.random()+1);
-		xText.innerHTML=x;
-		yText.innerHTML=y;
-		i=Math.floor(4*Math.random());
-		cal.innerHTML=calD[i];
-
-		switch(calD[i]){
-			case "+":
-				correct=x+y;
-				break;
-			case "-":
-				correct=x-y;
-				break;
-			case "x":
-				correct=x*y;
-				break;
-			case "/":
-				correct=Math.round(x/y*100)/100;
-				break;
-		}
-		let correct_index=choiceArr[Math.floor(4*Math.random())];
-		choices.childNodes[correct_index].innerHTML=correct;
-		for(let elem of choices.childNodes){
-			if(!elem.innerHTML){
-				if(correct%1!==0){
-					elem.innerHTML=Math.floor(100*Math.random()-10)/10;
-				}else{
-					elem.innerHTML=Math.floor(100*Math.random()-10);
-				}
-			}
-		}
-		console.log(correct);
-		console.log(choices.childNodes[1]);
-
+		newGame();
+		choosing();
 
 
 		startReset.innerText="Reset Game";
 		seconds.innerText="60";
 		countDown();
 	}else{
-		playing=false;
-		formula.innerHTML="";
-		startReset.innerText="Start Game";
+		
 		location.reload();
 
 	}
@@ -87,6 +55,93 @@ const stopTime=(time,action)=>{
 		clearInterval(action);
 		gameOver.style.display="flex";
 		gameOver.childNodes[3].childNodes[1].innerHTML=score;
+		game_Over();
 		
+	}
+}
+
+//Setting new Game within time frame:
+
+function newGame(){
+
+	wrong.style.display="none";
+
+	chosenFlag=false;
+	x=Math.floor(20*Math.random());
+		y=Math.floor(20*Math.random()+1);
+		xText.innerHTML=x;
+		yText.innerHTML=y;
+		i=Math.floor(4*Math.random());
+		cal.innerHTML=calD[i];
+
+		switch(calD[i]){
+			case "+":
+				correct=x+y;
+				break;
+			case "-":
+				correct=x-y;
+				break;
+			case "x":
+				correct=x*y;
+				break;
+			case "/":
+				correct=Math.round(x/y*100)/100;
+				break;
+		}
+		var correct_index=choiceArr[Math.floor(4*Math.random())];
+		choices.childNodes[correct_index].innerHTML=correct;
+
+		for(let elem of choices.childNodes){
+			//Setting the right value:
+			if(elem.innerHTML!==String(correct)){
+				if(correct%1!==0){
+					var choiceValue=Math.floor(100*Math.random()-10)/10;
+					if(choiceValue===correct){
+						choiceValue+=3;
+					}
+					elem.innerHTML=choiceValue;
+				}else{
+					var choiceValue=Math.floor(100*Math.random()-10);
+					if(choiceValue===correct){
+						choiceValue+=3;
+					}
+					elem.innerHTML=choiceValue;
+				}
+			}
+			
+		}
+}
+
+//game over
+
+function game_Over(){
+	chosenFlag=false;
+	correct=null;
+}
+
+//choosing function
+
+function choosing(){
+	for(let elem of choices.childNodes){
+		console.log("before choosing, the correct answer is "+correct);
+			elem.addEventListener("mousedown",function(){
+				if(elem.innerHTML===String(correct)&&chosenFlag===false){
+					score++;
+					chosenFlag=true;
+					scoreValue.innerHTML=score;
+					wrong.style.display="none";
+
+					console.log("correct="+correct+" and chosen="+elem.innerHTML);
+					newGame();
+				} else if(elem.innerHTML!==String(correct)&&chosenFlag===false){
+					console.log("correct answer is: "+correct+" and the chosen one is "+elem.innerHTML);
+					wrong.style.display="flex";
+					console.log("flex is somehow set!");
+				} else{
+					wrong.style.display="none";
+				}
+
+				
+			})
 	}
 }
